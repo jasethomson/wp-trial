db6a13e8c9d3a92707e4a77d160d6f775f66c64a
jasethomson <jasethomson31@gmail.com>
2020-01-29 12:33:48 -0800

    removed main from archive, index and archive-project to fix page size. added blog post page to single.php and added comment section to blog post page

--- wp-content/themes/custom/single.php
+++ wp-content/themes/custom/single.php
@@ -1,14 +1,55 @@
-<?php get_header(); ?>
-<?php
-while(have_posts()){
+<?php get_header();
+
+while (have_posts()) {
   the_post();
-  ?>
-  <h2><?php the_title();?></h2>
-  Posted by <?php the_author(); ?>
-  <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>">
-  <?php the_content();
-  comment_form();
-}
 
 ?>
-<?php get_footer(); ?>
+  <h2 class="page-heading"> <?php the_title(); ?></h2>
+  <div id="post-container">
+    <section id="blogpost">
+      <div class="card">
+        <div class="card-meta-blogpost">
+          Posted by <?php the_author(); ?> on <?php the_time('F j, Y'); ?> in
+          <a href="#"><?php echo get_the_category_list(', '); ?></a>
+        </div>
+        <div class="card-image">
+          <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="voyage">
+        </div>
+        <div class="card-description">
+          <?php the_content(); ?>
+        </div>
+      </div>
+      <div id="comments-section">
+        <?php comment_form();
+
+        $comments_number = get_comments_number();
+        if($comments_number !== 0){
+          ?>
+            <div class="comments">
+              <h3>What others say</h3>
+              <ol class="all-comments">
+                <?php
+                $comments = get_comments(array(
+                  'post_id' => $post->ID,
+                  'status' => 'approve'
+                ));
+                wp_list_comments(array(
+                  'per_page' => 15
+                ), $comments);
+                ?>
+              </ol>
+            </div>
+          <?php
+        }
+        ?>
+      </div>
+    </section>
+
+  <?php } ?>
+
+  <aside id="sidebar">
+    <h3>Sidebar Heading</h3>
+    <p>Sidebar heading</p>
+  </aside>
+  </div>
+  <?php get_footer(); ?>
