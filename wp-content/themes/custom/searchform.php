c789511e2fd3a3770b9b6f680c822a1010fec85f
jasethomson <jasethomson31@gmail.com>
2020-01-29 22:33:48 -0800

    fixed search input button

--- wp-content/themes/custom/searchform.php
+++ wp-content/themes/custom/searchform.php
@@ -1,4 +1,4 @@
 <form method="get" action="<?php echo home_url() ?>">
   <input type="search" name="s" placeholder="Search Here..." value="<?php echo get_search_query(); ?>">
-  <button type="submit"><i class="fas fa-search"></i></button>
+  <button id="search-btn" type="submit"><i class="fas fa-search"></i></button>
 </form>
