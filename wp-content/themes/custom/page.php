a629088e262d49a9dbc971b156a91e398a044cd8
jasethomson <jasethomson31@gmail.com>
2020-01-29 21:16:10 -0800

    added styling for contact form and added sidebar for contact

--- wp-content/themes/custom/page.php
+++ wp-content/themes/custom/page.php
@@ -24,8 +24,7 @@ while(have_posts()) {
   <?php } ?>
 
   <aside id="sidebar">
-    <h3>Sidebar Heading</h3>
-    <p>Sidebar heading</p>
+      <?php dynamic_sidebar('main_sidebar'); ?>
   </aside>
 </div>
 
